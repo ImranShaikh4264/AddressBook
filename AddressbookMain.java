98ef3249c598c113b75dd6d32f285895204187e2
ImranShaikh4264 <shaikhimrana56@gmail.com>
2022-10-25 11:07:12 +0530

    Added: Well come message for Address Book programs

---
+++ AddressbookMain.java
@@ -0,0 +1,10 @@
+package com.bridgelab.AddressBook;
+
+public class AddressbookMain {
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Address Book Programs");
+
+	}
+
+}
